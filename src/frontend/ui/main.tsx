9d7e2301c9db532792f821fb77614d28a5e3ec5f
RudyBlack <rudy1011100@gmail.com>
2025-12-23 20:18:32 +0900

    ..

--- src/frontend/ui/main.tsx
+++ src/frontend/ui/main.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { MemoryRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import { TestComp } from "./debug/TestComp";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HashRouter>
+    <MemoryRouter initialEntries={["/"]}>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/dev" element={<TestComp />} />
       </Routes>
-    </HashRouter>
+    </MemoryRouter>
   </React.StrictMode>
 );
